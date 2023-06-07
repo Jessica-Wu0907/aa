728540b3c2b56038a6fe2f788c9d009f94c7cb62
jessica740 <jessicawu740@gmail.com>
2023-06-07 15:14:46 -0400

    update
    update

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
+    <title>My Cv</title>
+</head>
+<body>
+    <header class="main-hearder">
+        <div class="container">
+            <div class="hearder-flex">
+
+                <div class="hamburger">
+					<i class="fa-solid fa-bars"></i>
+				</div>
+
+                <nav>
+                    <ul class="nav-list">
+                        <li>
+                            <a href="">home</a>
+                        </li>
+                        <li>
+                            <a href="">protfolio</a>
+                        </li>
+                        <li>
+                            <a href="">resume</a>
+                        </li>
+                        <li>
+                            <a href="">about</a>
+                        </li>
+                        <li>
+                            <a href="">contact</a>
+                        </li>
+
+                    </ul>
+
+
+                </nav>
+            </div>
+
+        </div>
+          <div class="big-banner">
+                <div class="textbox">
+                    <h2 class="big-hello">hello, i'm</h2>
+                    <strong><h1>Emily</h1></strong>
+                    <p>and this my rezume</p>
+        
+        
+                </div>
+            </div>
+    </header>
+
+    <section class="protfolio">
+        <div class="container">
+            <h3>featured <span style="font-weight: bold;">protfolio</span></h3>
+            <div class="protfolio-flex">
+
+                <div class="hamburger">
+					<i class="fa-solid fa-bars"></i>
+				</div>
+
+                <nav>
+                    <ul class="nav-list">
+                        <li>
+                            <a href=""><span style="color: rgb(238, 227, 26);">all</span></a>
+                        </li>
+                        <li>
+                            <a href="">Packaging</a>
+                        </li>
+                        <li>
+                            <a href="">Mockup</a>
+                        </li>
+                        <li>
+                            <a href="">Typgraphy</a>
+                        </li>
+                        
+                    </ul>
+                </nav>
+        </div>
+
+        <div class="container-card">
+            <div class="card-flex">
+                <div class="card">
+                    <div class="img1"></div>
+                    <div class="info">
+                        <h3>Packaging Brand</h3>
+                        <p>Packaging</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img2"></div>
+                    <div class="info">
+                        <h3>isometric 3D extrusion</h3>
+                        <p>Typgraphy</p>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div class="img3"></div>
+                    <div class="info">
+                        <h3>White Space Photograhy</h3>
+                        <p>photography</p>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+    </div>
+
+    </section>
+
+    <section class="about">
+        <div class="container">
+            <h3>my <span style="font-weight: bold;">resume</span></h3>
+            <div class="about-flex">
+                <div class="left">
+                    <div class="container1">
+                        <h2>Education</h2>
+                        <div class="education-flex">
+                            <div class="card1">
+                               <a class="icon-link" href=""><i class="fa-regular fa-calendar" style="color: #fcfcfc;"></i>March 2013 - Present</a> 
+                                <h4>Masteral In Information Technology</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <p>New York University</p>
+                            </div><!--end of card 01-->
+
+                            <div class="card2">
+                                <a class="icon-link" href=""><i class="fa-regular fa-calendar" style="color: #fcfcfc;"></i>March 2013 - Present</a> 
+                                <h4>Masteral In Information Technology</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <p>New York University</p>
+                            </div><!--end of card 02-->
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="right">
+                    <div class="container1">
+                        <h2>Experience</h2>
+                        <div class="exprience-flex">
+                            <div class="card1">
+                                <a class="icon-link" href=""><i class="fa-regular fa-calendar" style="color: #fcfcfc;"></i>March 2013 - Present</a> 
+                                <h4>Masteral In Information Technology</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <p>New York University</p>
+                            </div><!--end of card 01-->
+
+                            <div class="card2">
+                                <a class="icon-link" href=""><i class="fa-regular fa-calendar" style="color: #fcfcfc;"></i>March 2013 - Present</a> 
+                                <h4>Masteral In Information Technology</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                <p>New York University</p>
+                            </div><!--end of card 02-->
+
+                        </div>
+                    </div>
+                </div><!--end of right 02-->
+            </div>
+        </div>
+
+    </section>
+
+    <section class="me">
+        <div class="container">
+            <div class="me-flex">
+                <div class="me-left">
+                    <img src="img" alt="">
+                </div>
+                <div class="me-right">
+                    <h3 class="h3-me">about <span style="font-weight: bold;">me</span></h3>
+                    <h6>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, dicta.</h6>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab nihil ipsa deserunt ex cumque quam?</p>
+                    <a class="btn" href="">Hire me</a>
+                    <a class="btn2" href="">download CV</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="cta">
+        <div class="container">
+            <h3>get <span style="font-weight: bold;">in touch</span></h3>
+            <h2>my contact details</h2>
+            <h5>email</h5>
+            <p>jessicawu740@gmail.com</p>
+            <h5>phone</h5>
+            <p>514-996-0000</p>
+            <h5>address</h5>
+            <p class="sp">San Francisco street, Ma 50F</p>
+            <div class="social-media-list">
+                <a href="">
+                    <i class="fa-brands fa-instagram"></i>
+                </a>
+                <a href="">
+                    <i class="fa-brands fa-twitter"></i>
+                </a>
+                <a href="">
+                    <i class="fa-brands fa-youtube"></i>
+                </a>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
